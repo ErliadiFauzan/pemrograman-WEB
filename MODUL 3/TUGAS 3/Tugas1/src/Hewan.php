58a059e0cca73db305f9df1cec9bfcaf83f07e46
ErliadiFauzan <erliadi1122@gmail.com>
2024-10-31 17:37:08 +0700

    Menambahkan tugas 3

---
+++ MODUL 3/TUGAS 3/Tugas1/src/Hewan.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Project;
+
+use Project\Traits\SuaraTrait;
+use Project\Abstracts\HewanDasar;
+
+class Kucing extends HewanDasar
+{
+    use SuaraTrait;
+
+    public function bergerak(): string
+    {
+        return "Kucing ini berjalan dengan anggun.";
+    }
+
+    // Magic method __toString
+    public function __toString(): string
+    {
+        return "Ini adalah kucing bernama {$this->nama}, dari spesies {$this->spesies}.";
+    }
+}
+
+class Burung extends HewanDasar
+{
+    use SuaraTrait;
+
+    public function bergerak(): string
+    {
+        return "Burung ini sedang terbang.";
+    }
+
+    // Magic method __toString
+    public function __toString(): string
+    {
+        return "Ini adalah burung bernama {$this->nama}, dari spesies {$this->spesies}.";
+    }
+}
---
+++ MODUL 3/TUGAS 3/Tugas1/src/Traits/SuaraTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Project\Traits;
+
+trait SuaraTrait
+{
+    public function buatSuara(string $suara)
+    {
+        return "Hewan ini mengeluarkan suara {$suara}.";
+    }
+}
