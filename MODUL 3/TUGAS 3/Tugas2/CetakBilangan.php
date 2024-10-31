9bb08ebc424a99dbb11f29a2759833aca3ecbbe3
ErliadiFauzan <148746112+ErliadiFauzan@users.noreply.github.com>
2024-10-31 17:39:59 +0700

    Add files via upload
    tugas 3

---
+++ MODUL 3/TUGAS 3/Tugas2/CetakBilangan.php
@@ -0,0 +1,32 @@
+<?php
+
+function cetakBilangan($n) {
+
+    if ($n <= 0 || !is_int($n)) {
+        echo "Masukkan bilangan bulat positif.\n";
+        return;
+    }
+
+
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 4 == 0 && $i % 6 == 0) {
+
+            echo "Pemrograman Website 2024\n";
+        } elseif ($i % 5 == 0) {
+
+            echo "2024\n";
+        } elseif ($i % 4 == 0) {
+
+            echo "Pemrograman\n";
+        } elseif ($i % 6 == 0) {
+
+            echo "Website\n";
+        } else {
+
+            echo $i . "\n";
+        }
+    }
+}
+
+cetakBilangan(30);
+?>
